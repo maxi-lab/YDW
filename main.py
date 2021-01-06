eb3ebc6cab9714a1855f40132bc864a841919620
maxi-lab <76931520+maxi-lab@users.noreply.github.com>
2021-01-06 01:56:47 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import tkinter as tk
-
+import pytube
 
 if __name__ == "__main__":
     root=tk.Tk()
     root.title("Yout DW")
     root.minsize(300,300)
     tk.Label(root,text="Ingresa el link aquí").pack()
+    entrada=tk.Entry()
+    entrada.pack()
+    tk.Button(text="Descarga").pack()
     root.mainloop()
