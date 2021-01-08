7bd19a557d3378d09424527f1f96bb477d4e819c
maxi-lab <76931520+maxi-lab@users.noreply.github.com>
2021-01-07 21:12:07 -0300

    Add files via upload
    youtube_dw

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import tkinter as tk
-import pytube
+import youtube_dl
+
+def dw(url):
+    youtube_info=youtube_dl.YoutubeDL().extract_info(url=url, download=False)
+    file_name=f"{youtube_info['title']}.mp3"
+    options={
+        "format":"bestadio/best",
+        "keepvideo":False,
+        "outtmpl":file_name,
+        "postprocessors":[{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }]
+    }
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download([youtube_info['webpage_url']])
 
 if __name__ == "__main__":
     root=tk.Tk()
@@ -8,5 +24,5 @@ if __name__ == "__main__":
     tk.Label(root,text="Ingresa el link aquí").pack()
     entrada=tk.Entry()
     entrada.pack()
-    tk.Button(text="Descarga").pack()
+    tk.Button(text="Descarga", command=lambda : dw(entrada.get())).pack()
     root.mainloop()
